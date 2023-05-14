08534d406ed4a36ef566af9a77fa52b00e8fe99b
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-14 13:42:19 -0300

    make responsive pages

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -6,9 +6,9 @@ import instagram from '../assets/img/instagram96.png'
 const MainPage = () => {
   return (
      <>
-      <main className='bg-indigo-600 md:w-full h-32'>
+      <main className='bg-indigo-600 md:w-full md:h-32'>
         <div>
-          <h1 className="font-bold text-8xl pt-2 text-white text-center"
+          <h1 className="font-bold md:text-8xl pt-2 text-white text-center"
           >
             Cocchautos
           </h1>  
