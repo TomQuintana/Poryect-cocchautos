1fcde82112cdc2d40cdb2c3abf56a984e1fabaa9
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-14 11:06:03 -0300

    change nav into home page and add new services in servicio page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage/>}>
           <Route index element={<HomePage/>}/>
-          <Route path='contact' element={<Contact/>} />
+          <Route path='contacto' element={<Contact/>} />
           <Route path='servicios' element={<Servicios/>} />
         </Route>
       </Routes>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,35 +2,74 @@ import ReactPlayer from 'react-player'
 import product from '../assets/img/videoProducto.17.10.mp4'
 import antes from '../assets/img/antes.jpeg'
 import despues from '../assets/img/despues.jpeg'
-
+import { Link } from "react-router-dom"
+import whatsapp from '../assets/img/whatsapp.png'
 
 const HomePage = () => {
   return (
     <>
-      <div className='mb-8'>
-        <h1
-          className='text-center font-mono text-xl'
-        >Algunos Trabajos Realizados</h1>
+      <div className='flex justify-center'>
+        <div className='mx-auto shadow rounded-xl w-3/6 pb-4 pt-4'>
+          <h1
+            className='font-mono text-3xl text-center'
+          >Expertos en Detailing</h1>
+          <br/>
+          <p
+            className='font-mono text-center text-xl'
+          >Todo lo que estas buscando
+            para mejorar la estética de tu vehículo.</p>
+        </div>
       </div>
-      <div className="mt-10 flex justify-center md:w-full">
-        <div className=' flex justify-center w-3/6'>
-          <div className='shadow rounded-xl'>
-            <ReactPlayer
-              url={product}
-              controls
-              loop
-            />
+      <br/>
+      <br/>
+      <br/>
+      <div>
+        <h1
+          className='text-center font-mono text-2xl'
+        >Contamos con los siguientes servicios entre otros</h1>
+        <div className='mt-5 flex justify-between container mx-auto'>
+          <div className='w-96 h-60 shadow rounded-xl p-5'>
+            <h1
+              className='font-mono'
+            >Tratamientos acrílicos, cerámicos y de vidrio líquido</h1>
+            <img 
+              className='w-40'
+              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/003-automobile-red.svg"/>
+          </div>
+          <div className='w-96 h-60 shadow rounded-xl p-5'>
+            <h1
+              className='font-mono'
+            >Tratamiento de interiores de vehículos (tapizados)</h1>
+            <img 
+              className='w-40'
+              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/009-car-1-red.svg"/>
+          </div>
+          <div className='w-96 h-60 shadow rounded-xl p-5'>
+            <h1
+              className='font-mono'
+            >Pulido de Opticas</h1>
+            <img 
+              className='w-40'
+              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/022-protection-red.svg"/>
           </div>
         </div>
-      </div>
-      <div className='md:container mx-auto items-center flex justify-center gap-28 mt-10 '>
-        <div className='md:w-80 shadow rounded-xl'>
-          <h1 className='text-center font-mono text-2xl'>Antes</h1>
-          <img src={ antes } alt="" />
-        </div>
-        <div className='w-80 shadow rounded-xl font-mono text-2xl'>
-          <h1 className='text-center'>Despues</h1>
-          <img src={ despues  }alt="" />
+        <div className='mt-10 flex justify-center gap-2'>
+          <div>
+            <h1
+              className='text-center text-xl font-mono'
+            >Para mas informacion puedes ponerte en contacto con nosotros {''}
+            </h1> 
+          </div>
+          <div>
+            <a 
+              className='mr-2 '
+              href="https://api.whatsapp.com/send?phone=1168386465">
+              <img 
+                className="rounded hover:bg-gray-200"
+                src={ whatsapp } 
+                alt="Github"/>
+            </a>
+          </div>
         </div>
       </div>
     </>
