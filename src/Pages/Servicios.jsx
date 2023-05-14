950eff15dd21d7e6d7a77615aea4c23fc3e8b4da
tomasquintana-gj <tomquintana20@gmail.com>
2023-05-14 13:33:02 -0300

    add new images for servicios page

--- src/Pages/Servicios.jsx
+++ src/Pages/Servicios.jsx
@@ -1,3 +1,9 @@
+import asiento from '../assets/img/asiento100.png'
+import servicioDerecha from '../assets/img/servicioCoche96.png'
+import limpiezaOpticas from '../assets/img/limpiezaAutos64.png'
+import limpiezaBrillantado from '../assets/img/limpiezaAutos100.png'
+import limpiezaAuto from '../assets/img/servicioCoche100.png'
+import pulido from '../assets/img/polisher.png'
 
 const Servicios = () => {
   return (
@@ -12,51 +18,51 @@ const Servicios = () => {
               className='font-mono'
             >Tratamientos acrílicos, cerámicos y de vidrio líquido</h1>
             <img 
-              className='w-40'
-              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/003-automobile-red.svg"/>
+              className='w-32'
+              alt="null" src={servicioDerecha}/>
           </div>
-          <div className='w-96 h-60 shadow rounded-xl p-5'>
+          <div className='w-96 h-60 shadow rounded-xl p-5 items-center'>
             <h1
               className='font-mono'
             >Tratamiento de interiores de vehículos (tapizados)</h1>
             <img 
-              className='w-40'
-              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/009-car-1-red.svg"/>
+              className='w-32'
+              alt="null" src={asiento}/>
           </div>
           <div className='w-96 h-60 shadow rounded-xl p-5'>
             <h1
               className='font-mono'
             >Pulido de Opticas</h1>
             <img 
-              className='w-40'
-              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/022-protection-red.svg"/>
+              className='w-28'
+              alt="null" src={limpiezaOpticas}/>
           </div>
         </div>
         <div className='mt-5 flex justify-between container mx-auto'>
           <div className='w-96 h-60 shadow rounded-xl p-5'>
             <h1
               className='font-mono'
-            >Pulido de ópticas</h1>
+            >Pulido en tres Pasos</h1>
             <img 
-              className='w-40'
-              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/003-automobile-red.svg"/>
+              className='w-28'
+              alt="null" src={pulido}/>
           </div>
           <div className='w-96 h-60 shadow rounded-xl p-5'>
             <h1
               className='font-mono'
             >Abrillantado</h1>
             <img 
-              className='w-40'
-              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/009-car-1-red.svg"/>
+              className='w-32'
+              alt="null" src={limpiezaBrillantado}/>
           </div>
           <div className='w-96 h-60 shadow rounded-xl p-5'>
             <h1
               className='font-mono'
-            >Hago pulido en tres pasos 
+            >Restauración y pintura llantas 
             </h1>
             <img 
-              className='w-40'
-              alt="null" src="https://russcar.com/wp-content/uploads/2021/01/022-protection-red.svg"/>
+              className='w-32'
+              alt="null" src={limpiezaAuto}/>
           </div>
         </div>
       </div>
